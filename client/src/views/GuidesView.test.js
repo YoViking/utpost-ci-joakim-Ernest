00d6305da199c4f78ae8801e4b6163a54583ec79
YoViking <joakim.viking@gmail.com>
2026-09-25 11:53:46 +0200

    delete

--- client/src/views/GuidesView.test.js
+++ client/src/views/GuidesView.test.js
@@ -22,7 +22,7 @@ describe('GuidesView', () => {
       global: { stubs: { RouterLink: { template: '<a><slot /></a>' } } },
     })
     // await flushPromises()
-    // expect(wrapper.text()).toContain('Kebnekaise')
+    // // expect(wrapper.text()).toContain('Kebnekaise')
     // expect(wrapper.text()).toContain('1 av 1')
   })
 })
