7603864c64bd84a6ac1baebee4e6cf92461699f2
YoViking <joakim.viking@gmail.com>
2026-09-25 11:15:04 +0200

    fuck up

--- api/src/db/client.js
+++ api/src/db/client.js
@@ -3,6 +3,8 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import { config } from '../config.js';
 import * as schema from './schema.js';
 
+const Konstigvariabel = null
+
 const { Pool } = pkg;
 
 export const pool = new Pool({ connectionString: config.databaseUrl });
